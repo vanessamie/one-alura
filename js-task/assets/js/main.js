203ccc22d4c74022096443d91f721106f7a3aa38
Mie <n.vanessamie@gmail.com>
2022-07-27 20:03:13 -0300

    alterações para troca de local

--- js-task/assets/js/main.js
+++
@@ -1,8 +0,0 @@
-import { novoItem } from "./criaTarefa.js";
-import { carregaTarefa } from "./carregaTarefa.js";
-
-const btnAdicionarTarefa = document.querySelector('[data-form-button]');
-
-btnAdicionarTarefa.addEventListener('click', novoItem)
-
-carregaTarefa()
